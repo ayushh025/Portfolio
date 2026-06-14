16e1a5626891092fac22f36bffc64c76e6a29812
ayushh025 <ayushprj12@gmail.com>
2026-06-14 12:41:07 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Hero from "./components/utils/Hero";
 import Navbar from "./components/utils/Navbar";
-
+import Hero from "./components/utils/Hero";
+import About from "./components/utils/About";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 };
---
+++ src/components/utils/About.jsx
@@ -0,0 +1,56 @@
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-20 px-5 bg-linear-to-r from-[#01191d] to-[#022c2b]"
+    >
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-2 text-gray-300">
+          About <span className="text-outline">Me</span>
+        </h2>
+        <p className="text-gray-500 text-center mb-12">
+          Get to know more about me
+        </p>
+        <div className="bg-[#022c2b] backdrop-blur-md rounded-2xl p-8 md:p-12 grid grid-cols-1 lg:grid-cols-2 gap-12 border border-white/20">
+          <div className="">
+            <h3 className="text-xl font-semibold mb-4 text-white">Who am I?</h3>
+            <p className="text-slate-300 mb-4">
+              I'm a passionate Frontend Developer with a strong foundation in
+              HTML, CSS, JavaScript, React.js, and responsive web design. I
+              enjoy turning ideas into interactive, user-friendly web
+              applications while focusing on clean code and modern development
+              practices.
+            </p>
+            <p className="text-slate-300">
+              I believe in continuous learning and consistently work on
+              improving my skills by building projects, exploring new
+              technologies, and solving real-world problems. My goal is to grow
+              as a MERN Stack Developer and contribute to impactful digital
+              experiences.
+            </p>
+          </div>
+          <div className="mt-8 grid grid-cols-2 gap-4 text-gray-300">
+            <div>
+              <p>Name:</p>
+              <p className="font-medium text-white">Ayush Prajapati</p>
+            </div>
+            <div>
+              <p>E-mail:</p>
+              <p className="font-medium text-white">ayushprj22@gmail.com</p>
+            </div>
+            <div>
+              <p>Mobile No:</p>
+              <p className="font-medium text-white">+91 9737334436</p>
+            </div>
+            <div>
+              <p>Availablity:</p>
+              <p className="font-medium text-white">Open to Work</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
